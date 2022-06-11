06071b6b4b827cb9f448515511e38fce25a0a04e
Semolik <70706334+Semolik@users.noreply.github.com>
2022-06-11 18:12:53 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/static/favicon.png" type="image/png" sizes="16x16" />
+        <link rel="icon" href="/favicon.png" type="image/png" sizes="16x16" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover" />
       </Head>
       <Component {...pageProps} />
