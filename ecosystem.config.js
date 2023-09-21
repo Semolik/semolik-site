aba675ce9ffa5f9563516a469fecd907d8340755
Semolik <70706334+Semolik@users.noreply.github.com>
2023-09-22 01:36:56 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: "semolik-site",
-            port: "3001",
+            port: "3000",
             exec_mode: "cluster",
             instances: "max",
             script: "./.output/server/index.mjs",
